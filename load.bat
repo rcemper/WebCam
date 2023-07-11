492adf59b1dc99ef47cee3673cb3d2857a098681
rcemper <31236645+rcemper@users.noreply.github.com>
2023-07-12 01:07:23 +0200

    Add files via upload

---
+++ load.bat
@@ -0,0 +1,31 @@
+@echo off
+rem :: aktuelles datum
+ set _dt=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%
+rem :: ein verzeichnis pro tag
+if NOT exist %_dt% (
+ md %_dt% 
+ echo -1 >%_dt%.txt
+)
+rem :: timeout zwischen bildern 
+ set _to=%1
+ if [%1]==[]  set /a _to=4
+rem :: dir %_dt%
+ set wget="C:\Program Files (x86)\GnuWin32\bin\wget"
+rem :: aufräumen
+ del /F /Q webcamimage.jp* >nul 2>&1
+:loop
+rem :: bild abholen
+ %wget% -q -o -nd http://www.lkexca.com/camakt/webcamimage.jpg >nul 2>&1
+rem :: nächste file id 5stellig führende 0
+ set /p _lin=<%_dt%.txt
+ set /a "_seq=1+_lin"
+ set _id1=x00000%_seq%
+ set _id=%_id1:~-5,5%
+rem :: aktuelle id merken
+ >%_dt%.txt echo %_seq%
+ set _fil=%_dt%\%_id%.jpg
+set _f
+rem :: file id ins tages-verzeichnis schieben
+ move webcamimage.jpg %_fil% > nul
+ timeout %_to% > nul
+goto loop
--- video32.bat
+++ video32.bat
@@ -1,16 +1,16 @@
 @echo off
-rem :: param %1 = framerate default=5
+rem :: param %1 = rate default=5
 set _fr=%1
-if [%1]==[]  set /A _fr=8
+if [%1]==[]  set /A _fr=150
 echo *** video generieren ***
 set _day=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%
 dir /b /A:D
 echo| set /p _p="welcher tag ? (yyyy-mm-dd) [ %_day% ]: "<nul 
 set /p _day=<CON
 cd %_day%
-echo video %_day% wird erstellt in %CD% mit %_fr% frame/sec
+echo video %_day% wird erstellt in %CD% mit rate %_fr% 
 pause
-2>nul C:\WebCam\ffmpeg32 -framerate %_fr% -y  %_day%.mp4 -i "%%05d.jpg"
+2>nul C:\WebCam\ffmpeg32 -r %_fr% -y  %_day%.mp4 -i "%%05d.jpg" 
 dir /b *.mp4
 cd ..
 echo fertig
